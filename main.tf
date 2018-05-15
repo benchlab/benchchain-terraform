77a657163d21c4b0d2f6e0402b6aaf32ccc24c78
BenchLabs <38194539+benchlab@users.noreply.github.com>
2018-05-15 18:14:55 -0500

    added main terraform config

---
+++ main.tf
@@ -0,0 +1,36 @@
+#BenchChain Terraform Configuration
+
+variable "API" {
+  description = "DigitalOcean API Access Token"
+}
+
+variable "BENCH_TESTNET_NAME" {
+  description = "Name of the BenchChain TestNet or BenchChain-related sidechain or dappchain to be deployed. "
+  default = "benchchain-testnet"
+}
+
+variable "KEY" {
+  description = "Your SSH public key to be distributed amongst all BenchChain nodes."
+  type = "string"
+}
+
+variable "BNODECOUNT" {
+  description = "Number of BenchChain Nodes To Deploy"
+  default = "4"
+}
+
+provider "digitalocean" {
+  token = "${var.API}"
+}
+
+module "benchcluster" {
+  source           = "./benchcluster"
+  name             = "${var.BENCH_TESTNET_NAME}"
+  ssh_key          = "${var.KEY}"
+  servers          = "${var.SERVERS}"
+}
+
+
+output "public_ips" {
+  value = "${module.benchcluster.public_ips}"
+}
